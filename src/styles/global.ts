ba3c319105cf4c5e20026620d08445bd90ba69b4
jgabriel1 <josegabriel@det.ufc.br>
2021-03-15 18:55:24 -0300

    Created summary visual component.

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,6 +4,7 @@ export const GlobalStyle = createGlobalStyle`
   :root {
     --background: #f0f2f5;
     --red: #e52e4d;
+    --green: #33cc95;
     --blue: #5429cc;
 
     --blue-light: #6933ff;
@@ -35,7 +36,7 @@ export const GlobalStyle = createGlobalStyle`
     background: var(--background);
     -webkit-font-smoothing: antialiased;
   }
-  
+
   body, input, textarea, button {
     font-family: 'Poppins', sans-serif;
     font-weight: 400;
