1fb773a46743ffabeb54c09e3195da6483079fa6
jgabriel1 <josegabriel@det.ufc.br>
2021-03-16 18:25:46 -0300

    Created and styled the entire modal form.

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,9 @@
 import Modal from 'react-modal';
-import { Container } from "./styles";
+import { Container, TransactionTypeContainer } from './styles';
+
+import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
 
 type NewTransactionModalProps = {
   isOpen: boolean;
@@ -11,13 +15,45 @@ export function NewTransactionModal({
   onRequestClose,
 }: NewTransactionModalProps) {
   return (
-    <Container>
-      <Modal
-        isOpen={isOpen}
-        onRequestClose={onRequestClose}
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName='react-modal-overlay'
+      className='react-modal-content'
+    >
+      <button
+        type='button'
+        onClick={onRequestClose}
+        className='react-modal-close'
       >
+        <img src={closeImg} alt="X" />
+      </button>
+
+      <Container>
         <h2>Cadastrar transação</h2>
-      </Modal>
-    </Container>
+
+        <input placeholder='Título' />
+
+        <input placeholder='Valor' type='number' />
+
+        <TransactionTypeContainer>
+          <button type='button'>
+            <img src={incomeImg} alt='Entrada' />
+
+            <span>Entrada</span>
+          </button>
+
+          <button type='button'>
+            <img src={outcomeImg} alt='Saída' />
+
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+
+        <input placeholder='Categoria' />
+
+        <button type='submit'>Cadastrar</button>
+      </Container>
+    </Modal>
   );
 }
